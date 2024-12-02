dd6eb3a1fabc56ec5b7a26befebd94467401387e
Natan <npolenec@gmail.com>
2024-12-02 17:24:38 +0100

    day2 p1

--- app/Days/Day2.hs
+++ app/Days/Day2.hs
@@ -1,10 +1,27 @@
 module Days.Day2 (runner) where
 import Types
 
-runner :: ToRun -> String -> (String, String)
-runner runs cont = 
-    let 
-    in (ifFst runs undefined,
-        ifSnd runs undefined
+-- could literally just be a tuple but eh
+data ScanStep = ScanStep {delta :: Int, curr :: Int, valid :: Bool}
+
+bti x = if x then 1 else 0
+isEntryListValid le = case dropWhile valid le of
+    [] -> True
+    (ScanStep{valid=False}):xs -> False
+    _ -> False
+
+
+--runner :: ToRun -> String -> (String, String)
+runner runs cont =
+    let reports = map (map read . words) . lines $ cont
+        reports :: [[Int]]
+    in (ifFst runs $ show . sum . map (\rep -> bti . isEntryListValid $
+        scanl (\acc x ->
+            let prevNum = curr acc
+                diff = x - prevNum
+                isStepValid = abs diff >= 1 && abs diff <= 3 && signum diff + signum (delta acc) /= 0
+            in ScanStep{delta=diff,curr=x,valid=isStepValid}
+    ) (ScanStep{delta=0,curr=head rep,valid=True}) (tail rep)) $ reports,
+        ifSnd runs $ undefined
     )
 
