86204f8a8b80fdb6946d52f2f9a69006a6f4aecc
Natan <npolenec@gmail.com>
2024-12-02 21:37:53 +0100

    day2 p2 (bruteforce)

--- app/Days/Day2.hs
+++ app/Days/Day2.hs
@@ -10,18 +10,23 @@ isEntryListValid le = case dropWhile valid le of
     (ScanStep{valid=False}):xs -> False
     _ -> False
 
+allButOne :: [a] -> [[a]]
+allButOne = helper []
+    where helper _ [] = []
+          helper prev (x:xs) = (prev ++ xs) : helper (prev ++ [x]) xs
 
 --runner :: ToRun -> String -> (String, String)
 runner runs cont =
     let reports = map (map read . words) . lines $ cont
         reports :: [[Int]]
-    in (ifFst runs $ show . sum . map (\rep -> bti . isEntryListValid $
-        scanl (\acc x ->
-            let prevNum = curr acc
-                diff = x - prevNum
-                isStepValid = abs diff >= 1 && abs diff <= 3 && signum diff + signum (delta acc) /= 0
-            in ScanStep{delta=diff,curr=x,valid=isStepValid}
-    ) (ScanStep{delta=0,curr=head rep,valid=True}) (tail rep)) $ reports,
-        ifSnd runs $ undefined
+        scanner rep = scanl (\acc x ->
+                let prevNum = curr acc
+                    diff = x - prevNum
+                    isStepValid = abs diff >= 1 && abs diff <= 3 && signum diff + signum (delta acc) /= 0
+                in ScanStep{delta=diff,curr=x,valid=isStepValid}
+            ) (ScanStep{delta=0,curr=head rep,valid=True}) (tail rep)
+
+    in (ifFst runs $ show . sum . map (\rep -> bti . isEntryListValid $ scanner rep) $ reports,
+        ifSnd runs $ show . sum . map (\rep -> bti $ any (isEntryListValid . scanner) (rep:allButOne rep)) $ reports
     )
 
