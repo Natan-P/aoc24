5e6fdad94470dddc29f50bf07dfd32204ea4f780
Natan <npolenec@gmail.com>
2024-12-03 18:38:55 +0100

    day3 p1 (custom regex parser lol)

--- app/Days/Day3.hs
+++ app/Days/Day3.hs
@@ -1,10 +1,26 @@
 module Days.Day3 (runner) where
 import Types
+import Data.Char (isDigit)
+
+match :: String -> String -> [[String]]
+match initPat x = matcher' [[]] initPat x
+    where
+        matcher' [] pat ent = matcher' [[]] pat ent
+        matcher' matches [] s = matcher' ([]:matches) initPat s
+        matcher' (_:matches) _ [] = matches
+        matcher' matches@(latestMatch:tailMatches) pat@(matchChar:pat') entireStr@(c:str)
+            | matchChar == 'I' = if isDigit c 
+                                     then matcher' ((latestMatch++[[c]]):tailMatches) ('C':pat') str
+                                     else matcher' tailMatches initPat str
+            | matchChar == 'C' = if isDigit c
+                                     then matcher' ((init latestMatch++[(last latestMatch++[c])]):tailMatches) pat str
+                                     else matcher' matches pat' entireStr
+            | otherwise        = if c == matchChar then matcher' matches pat' str else matcher' ([]:tailMatches) initPat str
 
 runner :: ToRun -> String -> (String, String)
 runner runs cont = 
     let 
-    in (ifFst runs undefined,
+    in (ifFst runs $ show . sum . map (product . map read) $ match "mul(I,I)" cont,
         ifSnd runs undefined
     )
 
